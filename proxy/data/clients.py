5b187268cabe6673fdea2e20b8f5b6e1d7dd0c75
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-07-24 00:51:39 -0700

    Be more verbose for debugging

--- proxy/data/clients.py
+++ proxy/data/clients.py
@@ -15,12 +15,15 @@ class ClientData(object):
 
 def addClient(handle):
 	connectedClients[handle.playerId] = ClientData(handle.transport.getPeer().host, handle.myUsername, handle)
+	print('[Clients] Registered client %s (ID:%i) in online clients' % (handle.myUsername, handle.playerId))
 
 def removeClient(handle):
+	print("[Clients] Removing client %s (ID:%i) from online clients" % (handle.myUsername, handle.playerId))
 	del connectedClients[handle.playerId]
 
 def populateData(handle):
 	cData = connectedClients[handle.playerId]
+	cData.handle = handle
 	handle.myUsername = cData.segaId
 	if handle.transport.getHost().port in blocks.blockList:
 		bName = blocks.blockList[handle.transport.getHost().port][1]
