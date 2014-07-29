6c5a3bc52d3958c3c8ed5e7dffdbad9dfccd53ea
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-07-28 23:02:29 -0700

    Use datetime for timestamp

--- proxy/plugins/WebAPI.py
+++ proxy/plugins/WebAPI.py
@@ -1,13 +1,13 @@
 import data.clients, data.players, data.blocks 
 from twisted.web.resource import Resource
-import json, time
+import json, calendar, datetime
 
-upStart = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
+upStart = calendar.timegm(datetime.datetime.utcnow().utctimetuple())
 
 class WebAPI(Resource):
 	isLeaf = True
 
 	def render_GET(self, request):
-		currData = {'count' : len(data.clients.connectedClients), 'blocksCached' : len(data.blocks.blockList), 'playersCached' : len(data.players.playerList), 'upSince' : upStart}
+		currData = {'playerCount' : len(data.clients.connectedClients), 'blocksCached' : len(data.blocks.blockList), 'playersCached' : len(data.players.playerList), 'upSince' : upStart}
 		request.setHeader("content-type", "text/plain")
 		return json.dumps(currData)
