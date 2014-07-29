6c0e0e10605e9264a74277c76bcc5df0e8496116
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-07-28 22:59:24 -0700

    set leaf

--- proxy/plugins/WebAPI.py
+++ proxy/plugins/WebAPI.py
@@ -5,6 +5,9 @@ import json, time
 upStart = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
 
 class WebAPI(Resource):
+	isLeaf = True
+
 	def render_GET(self, request):
 		currData = {'count' : len(clients.connectedClients), 'blocksCached' : len(blocks.blockList), 'playersCached' : len(players.playerList), 'upSince' : upStart}
+		request.setHeader("content-type", "text/plain")
 		return json.dumps(currData)
