a91df264d7e7523d0d9d598f7f571289dfad52b9
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-07-30 05:11:25 -0700

    Add skeleton for redpill.py

---
+++ proxy/plugins/redpill.py
@@ -0,0 +1,7 @@
+# redpill.py PSO2Proxy plugin
+# For use with redpill.py flask webapp and website for packet logging and management
+import sqlite
+
+dbLocation = '/var/pso2-www/redpill/redpill.db'
+
+#TODO
