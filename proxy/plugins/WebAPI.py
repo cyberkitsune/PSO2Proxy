7fc487fc4a86c4d8405c9cfcfeeab722f3187c74
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-07-30 02:08:30 -0700

    Convert WebAPI to a plugin

--- proxy/plugins/WebAPI.py
+++ proxy/plugins/WebAPI.py
@@ -1,6 +1,8 @@
 import data.clients, data.players, data.blocks 
 from twisted.web.resource import Resource
-import json, calendar, datetime
+from twisted.internet import reactor
+from config import webapi as webapi
+import json, calendar, datetime, plugins
 
 upStart = calendar.timegm(datetime.datetime.utcnow().utctimetuple())
 
@@ -11,3 +13,12 @@ class WebAPI(Resource):
 		currData = {'playerCount' : len(data.clients.connectedClients), 'blocksCached' : len(data.blocks.blockList), 'playersCached' : len(data.players.playerList), 'upSince' : upStart}
 		request.setHeader("content-type", "application/json")
 		return json.dumps(currData)
+
+@plugins.onStartHook
+def setupWebAPI():
+	if webapi:
+		from twisted.web import server
+		import plugins.WebAPI as jsonsite
+		wEndpoint = TCP4ServerEndpoint(reactor, 8080, interface=ifaceIp)
+		wEndpoint.listen(server.Site(jsonsite.WebAPI()))
+	
