3843e3c43aee5abce6d20a92f0af773f9f6cad5c
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-08-06 00:51:19 -0700

    Spell this right

--- proxy/plugins/disabled/Translate.py
+++ proxy/plugins/disabled/Translate.py
@@ -14,29 +14,29 @@ translator = Translator(translation_config.get_key('app_id'), translation_config
 @p.on_connection_hook
 def create_preferences(client):
     if client.playerId in data.clients.connectedClients:
-        user_prefs = data.clients.connectedClients[client.playerId].get_prefrences()
+        user_prefs = data.clients.connectedClients[client.playerId].get_preferences()
         if 'translate_chat' not in user_prefs:
             user_prefs['translate_chat'] = False
-            data.clients.connectedClients[client.playerId].set_prefrences(user_prefs)
+            data.clients.connectedClients[client.playerId].set_preferences(user_prefs)
 
 
 @p.CommandHook("translate", "Toggles proxy-end chat translation. (Powered by Bing Translate, Incoming only.)")
 class ToggleTranslate(Command):
     def call_from_client(self, client):
         if client.playerId in data.clients.connectedClients:
-            user_prefs = data.clients.connectedClients[client.playerId].get_prefrences()
+            user_prefs = data.clients.connectedClients[client.playerId].get_preferences()
             user_prefs['translate_chat'] = not user_prefs['translate_chat']
             if user_prefs['translate_chat']:
                 client.send_crypto_packet(packetFactory.SystemMessagePacket("[Translate] Enabled Chat Translation.", 0x3).build())
             else:
                 client.send_crypto_packet(packetFactory.SystemMessagePacket("[Translate] Disabled Chat Translation.", 0x3).build())
-            data.clients.connectedClients[client.playerId].set_prefrences(user_prefs)
+            data.clients.connectedClients[client.playerId].set_preferences(user_prefs)
 
 
 @p.PacketHook(0x7, 0x0)
 def get_chat_packet(context, packet):
     if context.playerId in data.clients.connectedClients:
-        user_prefs = data.clients.connectedClients[context.playerId].get_prefrences()
+        user_prefs = data.clients.connectedClients[context.playerId].get_preferences()
         if not user_prefs['translate_chat']:
             return packet
     player_id = struct.unpack_from("<I", packet, 0x8)
