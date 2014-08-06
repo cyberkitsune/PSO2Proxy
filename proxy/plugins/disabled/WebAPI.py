e49a3fbdc81f14fd07e1ee6c4e0a79644239688e
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-08-05 17:34:25 -0700

    Rcon

--- proxy/plugins/disabled/WebAPI.py
+++ proxy/plugins/disabled/WebAPI.py
@@ -105,6 +105,7 @@ def setup_web_api():
     web_resource = WebAPI()
     web_resource.putChild("config.json", JSONConfig())
     web_resource.putChild("publickey.blob", PublicKey())
+    web_resource.putChild("rcon", WEBRcon())
     web_endpoint.listen(server.Site(web_resource))
 
 
