32370730d9c2b8eef3f7b2bef9ee84b5805f8ec8
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-08-06 01:34:17 -0700

    Not sure what is wrong here.

--- proxy/commands.py
+++ proxy/commands.py
@@ -343,7 +343,6 @@ class GlobalMessage(Command):
         if len(self.args.split(' ', 1)) < 2:
             client.send_crypto_packet(packetFactory.SystemMessagePacket("[ShipProxy] {red}Incorrect usage. Usage: |globalmsg  [message_type] <Message>", 0x3).build())
             return
-
         try:
             mode = int(self.args.split(' ', 2)[1])
         except ValueError:
