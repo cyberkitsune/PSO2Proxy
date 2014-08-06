43222c765848de673d7b9b20b88d6114d5b93769
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-08-05 19:31:09 -0700

    Git build

--- proxy/plugins/LoginMessage.py
+++ proxy/plugins/LoginMessage.py
@@ -2,15 +2,19 @@ import config
 import data.clients
 import plugins
 import packetFactory
+import subprocess
+
 from commands import Command
 
 
-login_config = config.YAMLConfig("cfg/loginmessage.config.yml", {'message': "{{yel}}Welcome to PSO2Proxy, {client_name}! There are currently {client_count} clients connected. Use {command_prefix}help for help!", 'messageType': 0x3}, True)
+login_config = config.YAMLConfig("cfg/loginmessage.config.yml", {'message': "{{yel}}Welcome to PSO2Proxy build {proxy_ver} {client_name}! There are currently {client_count} clients connected. Use {command_prefix}help for help!", 'messageType': 0x3}, True)
+cur_ver = subprocess.check_output(["git", "describe", "--always"])
 
 
 @plugins.on_connection_hook
 def login_message(sender):
-    message = login_config.get_key('message').format(client_name=sender.myUsername, client_count=len(data.clients.connectedClients), command_prefix=config.globalConfig.get_key('commandPrefix'))
+    global cur_ver
+    message = login_config.get_key('message').format(client_name=sender.myUsername, client_count=len(data.clients.connectedClients), proxy_ver=cur_ver, command_prefix=config.globalConfig.get_key('commandPrefix'))
     sender.send_crypto_packet(packetFactory.SystemMessagePacket(message, login_config.get_key('messageType')).build())
 
 
