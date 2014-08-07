997f827db60ca37a1db79cbaafeb130d85ff4da3
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-08-07 15:55:35 -0700

    Be more efficant here..

--- proxy/plugins/disabled/PacketLogger.py
+++ proxy/plugins/disabled/PacketLogger.py
@@ -52,7 +52,9 @@ def on_packet_received(context, packet, packet_type, packet_subtype):
     """
     :type context: ShipProxy
     """
-    if context.myUsername is not None and data.clients.ClientPreferences(context.myUsername)['logPackets'] is not None and not data.clients.ClientPreferences(context.myUsername)['logPackets']:
+    if 'prefs' not in context.extendedData and context.myUsername is not None:
+        context.extendedData['prefs'] = data.clients.ClientPreferences(context.myUsername)
+    if context.myUsername is not None and context.extendedData['prefs']['logPackets'] is not None and not context.extendedData['prefs']['logPackets']:
         if 'orphans' in context.extendedData:
             print("[PacketLogger] %s has opted out of packet logging. Deleting orphans..." % context.myUsername)
             del context.extendedData['orphans']
