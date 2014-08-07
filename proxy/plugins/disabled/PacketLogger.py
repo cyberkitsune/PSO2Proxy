401dc6d31731cda76dc8da413d107f920ea031dc
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-08-07 16:07:59 -0700

    Derp

--- proxy/plugins/disabled/PacketLogger.py
+++ proxy/plugins/disabled/PacketLogger.py
@@ -54,9 +54,13 @@ def on_packet_received(context, packet, packet_type, packet_subtype):
     """
     if 'prefs' not in context.extendedData and context.myUsername is not None and context.playerId not in data.clients.connectedClients:
         context.extendedData['prefs'] = data.clients.ClientPreferences(context.myUsername)
+        prefs = context.extendedData['prefs']
     elif context.playerId in data.clients.connectedClients and 'prefs' in context.extendedData:
         del context.extendedData['prefs']
-    if context.myUsername is not None and context.extendedData['prefs']['logPackets'] is not None and not context.extendedData['prefs']['logPackets']:
+        prefs = data.clients.connectedClients[context.playerId].preferences
+    else:
+        prefs = None
+    if context.myUsername is not None and prefs['logPackets'] is not None and not prefs['prefs']['logPackets']:
         if 'orphans' in context.extendedData:
             print("[PacketLogger] %s has opted out of packet logging. Deleting orphans..." % context.myUsername)
             del context.extendedData['orphans']
