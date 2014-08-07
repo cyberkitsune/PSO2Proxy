06e561bd76bb4034d55cfdb7adfd63d4ca03d39d
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-08-07 16:13:26 -0700

    Cache the reference instead...

--- proxy/plugins/disabled/PacketLogger.py
+++ proxy/plugins/disabled/PacketLogger.py
@@ -54,11 +54,9 @@ def on_packet_received(context, packet, packet_type, packet_subtype):
     """
     if 'prefs' not in context.extendedData and context.myUsername is not None and context.playerId not in data.clients.connectedClients:
         context.extendedData['prefs'] = data.clients.ClientPreferences(context.myUsername)
-        prefs = context.extendedData['prefs']
-    if context.playerId in data.clients.connectedClients and 'prefs' in context.extendedData:
-        del context.extendedData['prefs']
-        prefs = data.clients.connectedClients[context.playerId].preferences
-    if context.myUsername is not None and prefs['logPackets'] is not None and not prefs['logPackets']:
+    elif context.playerId in data.clients.connectedClients and 'prefs' in context.extendedData:
+        context.extendedData['prefs'] = data.clients.connectedClients[context.playerId].preferences
+    if context.myUsername is not None and context.extendedData['prefs']['logPackets'] is not None and not context.extendedData['prefs']['logPackets']:
         if 'orphans' in context.extendedData:
             print("[PacketLogger] %s has opted out of packet logging. Deleting orphans..." % context.myUsername)
             del context.extendedData['orphans']
