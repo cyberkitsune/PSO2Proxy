a243b056cfad1181dc2a2a17461b37d03027b1cc
Alam Arias <Alam.GBC@gmail.com>
2014-08-09 12:53:50 -0400

    forget the :

--- proxy/plugins/disabled/Translate.py
+++ proxy/plugins/disabled/Translate.py
@@ -59,7 +59,7 @@ def get_chat_packet(context, packet):
             return
         channel_id = struct.unpack_from("I", packet, 0x14)[0]
         message = packet[0x1C:].decode('utf-16').rstrip("\0")
-        if message.find(en_magic)
+        if message.find(en_magic):
              return packet
         d = threads.deferToThread(generate_google_translate_message, player_id, channel_id, message, "ja", "en")
         d.addCallback(context.peer.send_crypto_packet)
@@ -75,7 +75,7 @@ def get_chat_packet(context, packet):
         message = packet[0x1C:].decode('utf-16').rstrip("\0")
         if message.startswith("/"):
             return packet  # Command
-        if message.find(en_magic)
+        if message.find(en_magic):
              return packet
         japanese = False
         for char in message:
