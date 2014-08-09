195b4aeb35df84511f6b65e9e570a425497774be
Alam Arias <Alam.GBC@gmail.com>
2014-08-09 08:27:48 -0400

    Only build packets once for action messages

--- proxy/plugins/GlobalChat.py
+++ proxy/plugins/GlobalChat.py
@@ -76,13 +76,14 @@ if ircMode:
         def action(self, user, channel, msg):
             if channel == self.factory.channel:
                 print("[GlobalChat] [IRC] * %s %s" % (user, replace_irc_with_pso2(msg).decode('utf-8')))
+                TCPacket = packetFactory.TeamChatPacket(self.get_user_id(user.split("!")[0]), "[GIRC] %s" % user.split("!")[0], "* %s%s" % (gchatSettings['prefix'], replace_irc_with_pso2(msg).decode('utf-8'))).build()
+                SMPacket = packetFactory.SystemMessagePacket("[GIRC] <%s> * %s" % (user.split("!")[0], "%s%s" % (gchatSettings['prefix'], replace_irc_with_pso2(msg).decode('utf-8'))), 0x3).build()
                 for client in data.clients.connectedClients.values():
                     if client.preferences.get_preference('globalChat') and client.get_handle() is not None:
                         if gchatSettings['displayMode'] == 0:
-                            client.get_handle().send_crypto_packet(
-                                packetFactory.TeamChatPacket(self.get_user_id(user.split("!")[0]), "[GIRC] %s" % user.split("!")[0], "* %s%s" % (gchatSettings['prefix'], replace_irc_with_pso2(msg).decode('utf-8'))).build())
+                            client.get_handle().send_crypto_packet(TCPacket)
                         else:
-                            client.get_handle().send_crypto_packet(packetFactory.SystemMessagePacket("[GIRC] <%s> * %s" % (user.split("!")[0], "%s%s" % (gchatSettings['prefix'], replace_irc_with_pso2(msg).decode('utf-8'))), 0x3).build())
+                            client.get_handle().send_crypto_packet(SMpacket)
 
         def send_global_message(self, ship, user, message):
             self.msg(self.factory.channel, "[G-%02i] <%s> %s" % (ship, user, replace_pso2_with_irc(message)))
