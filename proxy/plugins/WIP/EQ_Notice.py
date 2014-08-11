c529d6d22bac96fb05918b82d840f7b537181af0
Alam Arias <Alam.GBC@gmail.com>
2014-08-10 23:19:45 -0400

    EQ_Notice Plugin: URL for each ship
       note: I can not select the "key"?

--- proxy/plugins/WIP/EQ_Notice.py
+++ proxy/plugins/WIP/EQ_Notice.py
@@ -9,49 +9,79 @@ from twisted.internet import task
 from twisted.internet import reactor
 from twisted.web.client import Agent, readBody
 from twisted.web.http_headers import Headers
+from config import globalConfig
 
 from commands import Command
 
-eqnotice_config = config.YAMLConfig("cfg/EQ_Notice.config.yml", {'enabled': True, 'timer' : 600, 'URL': "http://acf.me.uk/Public/PSO2EQ/pso2eq.txt" },  True)
+eqnotice_config = config.YAMLConfig("cfg/EQ_Notice.config.yml", {'enabled': True, 'timer' : 600
+,'0': ""
+,'1': "http://acf.me.uk/Public/PSO2EQ/pso2eq.txt"
+,'2': ""
+,'3': ""
+,'4': ""
+,'5': ""
+,'6': ""
+,'7': ""
+,'8': ""
+,'9': ""
+},  True)
 
-#stored_ETag = ''
-#stored_Last-Modified = ''
+
+ETag_Headers = ['','','','','','','','','','']
+Modified_Headers = ['','','','','','','','','','']
 
 eq_mode = eqnotice_config.get_key('enabled')
-eq_URL = eqnotice_config.get_key('URL')
 tasksec = eqnotice_config.get_key('timer')
 
 agent = Agent(reactor)
 
-#Sample: Ship02 08時00分【PSO2】第三採掘基地ダーカー接近予告
-#Sample 【1時間前】 Ship02 05時00分【PSO2】惑星リリーパ　作戦予告
+#Sample:	Ship02 08時00分【PSO2】第三採掘基地ダーカー接近予告
+#Sample:	【1時間前】 Ship02 05時00分【PSO2】惑星リリーパ　作戦予告
+#Sample:	【1時間前】 Ship02 11時00分【PSO2】旧マザーシップ　作戦予告
+#Sample:	Ship02 11時00分【PSO2】旧マザーシップ　作戦予告
 
-def cleanup_EQ(message):
+def cutup_EQ(message):
+    cutstr = u"分【PSO2】"
+    print u"Incoming message:  {}".format(message)
+    cutlen = message.rfind(cutstr)
+    if (cutlen is not -1):
+       message = message[cutlen+7:len(message)]
+    print u"Outgoing message:  {}".format(message)
     return message
 
+def cleanup_EQ(message):
+    return cutup_EQ(message)
+
 def EQBody(body, ship = 0):
-    body_utf8 = unicode(body, 'utf-8-sig', 'replace')
-    print("[EQ_Notice] %s" % (cleanup_EQ(body_utf8)))
-    SMPacket = packetFactory.SystemMessagePacket("[EQ_Notice] %s" % (cleanup_EQ(body_utf8)), 0x0).build()
+    body_utf8 = cleanup_EQ(unicode(body, 'utf-8-sig', 'replace'))
+    print("[EQ_Notice] Ship %02d : %s" % (ship, body_utf8))
+    SMPacket = packetFactory.SystemMessagePacket("[EQ_Notice] %s" % (body_utf8), 0x0).build()
     for client in data.clients.connectedClients.values():
-       if client.preferences.get_preference('eqnotice') and client.get_handle() is not None:
+       if client.preferences.get_preference('eqnotice') and client.get_handle() is not None and (ship == 0 or ship == data.clients.get_ship_from_port(cleint.transport.getHost().port)):
            client.get_handle().send_crypto_packet(SMPacket)
 
-def EQResponse(response, ship = 0):
+def EQResponse(response, ship = -1):
+    #print pformat(list(response.headers.getAllRawHeaders()))
+    ETag_Headers[ship-1] = response.headers.getRawHeaders('ETag')
+    Modified_Headers[ship-1] = response.headers.getRawHeaders('Last-Modified')
     d = readBody(response)
-    d.addCallback(EQBody, 0)
+    d.addCallback(EQBody, ship+1)
     return d
 
 def CheckupURL():
    HTTPHeader0 = Headers({'User-Agent': ['PSO2Proxy']})
-#
-#   if stored_ETag:
-#       HTTPHeader.addRawHeader("If-None-Match", stored_ETag)
-#   if stored_Last-Modified:
-#       HTTPHeader.addRawHeader("If-Modified-Since", stored_Last-Modified)
-#
-   EQ0 = agent.request('GET', eq_URL, HTTPHeader0, None)
-   EQ0.addCallback(EQResponse, 0)
+
+   for shipNum in config.globalConfig.get_key('enabledShips'):
+       #eq_URL = eqnotice_config.get_key(shipNum)
+       eq_URL = "http://acf.me.uk/Public/PSO2EQ/pso2eq.txt"
+       if eq_URL:
+          HTTPHeaderX = HTTPHeader0.copy()
+          if ETag_Headers[shipNum-1]:
+            HTTPHeaderX.addRawHeader('If-None-Match', ETag_Headers[shipNum-1])
+          if Modified_Headers[shipNum-1]:
+            HTTPHeaderX.addRawHeader('If-Modified-Since', Modified_Headers[shipNum-1])
+          EQ0 = agent.request('GET', eq_URL, HTTPHeaderX, None)
+          EQ0.addCallback(EQResponse, shipNum)
 
 @plugins.on_start_hook
 def on_start():
