74a6d73b5b5fdaa393f2158f256da61c6295d8d8
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-08-21 19:21:37 -0700

    Thread both query protocols off of the reactor thread to prevent blocking. Fixes #74

--- proxy/queryProtocols.py
+++ proxy/queryProtocols.py
@@ -1,4 +1,4 @@
-from twisted.internet import protocol
+from twisted.internet import protocol, threads
 import data.ships as ships
 from config import myIpAddress
 import plugins.plugins
@@ -12,7 +12,12 @@ class BlockScraper(protocol.Protocol):
     def connectionMade(self):
         port = self.transport.getHost().port
         print('[BlockQuery] %s:%i wants to load-balance on port %i!' % (self.transport.getPeer().host, self.transport.getPeer().port, port))
-        self.transport.write(ships.get_first_block(port, myIpAddress))
+        d = threads.deferToThread(ships.get_first_block, port, myIpAddress)
+        d.addCallback(self.send_block_scrape)
+        ships.get_first_block(port, myIpAddress)
+
+    def send_block_scrape(self, data):
+        self.transport.write(data)
         self.transport.loseConnection()
 
 
@@ -33,7 +38,11 @@ class ShipAdvertiser(protocol.Protocol):
         for f in plugins.plugins.onQueryConnection:
             f(self)
         print("[ShipStatus] Client connected " + str(self.transport.getPeer()) + "! Sending ship list packet...")
-        self.transport.write(ships.scrape_ship_packet("210.189.208.1", 12199, myIpAddress))
+        d = threads.deferToThread(ships.scrape_ship_packet, "210.189.208.1", 12199, myIpAddress)
+        d.addCallback(self.send_ship_list)
+
+    def send_ship_list(self, data):
+        self.transport.write(data)
         self.transport.loseConnection()
 
 
