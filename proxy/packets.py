32cbfae90c6c4cab25441bc5db0781cc786dc740
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-08-26 23:03:18 -0700

    Forgot a zero whoops

--- proxy/packets.py
+++ proxy/packets.py
@@ -40,7 +40,7 @@ class PacketHandler(object):
 @PacketHandler(0x11, 0x0)
 def login_packet(context, data):
     start = len(data) - 134  # Skip password
-    username = data[start:start + 0x4].decode('utf-8')
+    username = data[start:start + 0x40].decode('utf-8')
     username = username.rstrip('\0')
     print("[LoginPacket] Logging player %s in..." % username)
     context.myUsername = username
