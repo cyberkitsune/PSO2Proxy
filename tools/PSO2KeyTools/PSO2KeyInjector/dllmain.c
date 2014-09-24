73e14925c60346b88acf16adb9128e4822794c8e
Sean McClenaghan <cyberkitsune09@gmail.com>
2014-09-24 00:09:09 -0700

    Update RSA Key offset

--- tools/PSO2KeyTools/PSO2KeyInjector/dllmain.c
+++ tools/PSO2KeyTools/PSO2KeyInjector/dllmain.c
@@ -13,7 +13,7 @@
 #include <stdlib.h>
 
 // Constant variable(s):
-const unsigned char* RSAaddr = (unsigned char*)0x358D6A0;
+const unsigned char* RSAaddr = (unsigned char*)0x358D6C0;
 
 // Text to Inject failed to open publickey.blob
 LPCTSTR InjectText = "Failed to open publickey.blob";
