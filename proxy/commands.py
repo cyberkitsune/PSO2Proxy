3c26ba2ba0d0de16150b6a7a4a2402b65d495254
Ashley Scott <compaqtomania@live.co.uk>
2014-12-02 23:20:24 +0000

    Okay, this should do it.

--- proxy/commands.py
+++ proxy/commands.py
@@ -284,8 +284,8 @@ class Kick(Command):
                 packetFactory.SystemMessagePacket("[Command] {red}Invalid usage.\n(Usage: %skick <PlayerID>)" % config.globalConfig.get_key('commandPrefix'),
                                                   0x3).build())
             return
-        if int(args[1]) in data.clients.connectedClients:
-            if data.clients.connectedClients[int(args[1])].get_handle() is not None and type(args[1]) == int:
+        if type(args[1]) == int and int(args[1]) in data.clients.connectedClients:
+            if data.clients.connectedClients[int(args[1])].get_handle() is not None:
                 data.clients.connectedClients[int(args[1])].get_handle().send_crypto_packet(
                     packetFactory.SystemMessagePacket("[Proxy] {yel}You have been disconnected from the proxy by an admin.",
                                                       0x2).build())
@@ -302,8 +302,8 @@ class Kick(Command):
         args = self.args.split(' ')
         if len(args) < 2:
             return "[Command] Invalid usage. (Usage: kick <PlayerID>)"
-        if int(args[1]) in data.clients.connectedClients:
-            if data.clients.connectedClients[int(args[1])].get_handle() is not None and type(args[1]) == int:
+        if type(args[1]) == int and int(args[1]) in data.clients.connectedClients:
+            if data.clients.connectedClients[int(args[1])].get_handle() is not None:
                 data.clients.connectedClients[int(args[1])].get_handle().send_crypto_packet(
                     packetFactory.SystemMessagePacket("[Proxy] You have been disconnected from the proxy by an admin.", 0x1).build())
                 data.clients.connectedClients[int(args[1])].get_handle().transport.loseConnection()
