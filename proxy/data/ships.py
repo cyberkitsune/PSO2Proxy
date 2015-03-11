4be2914949e3715d304c303e470130a7619d498a
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2015-03-11 02:42:09 -0700

    sicknasty hack

--- proxy/data/ships.py
+++ proxy/data/ships.py
@@ -53,14 +53,14 @@ def get_first_block(ship_port, destination_ip):
         return None
 
     if ship_port not in cachedBlocks:
-        cachedBlocks[ship_port] = {'time_scraped': time.time(), 'data': scrape_block_packet(blockShipList[ship_port], ship_port, destination_ip)}
+        cachedBlocks[ship_port] = {'time_scraped': time.time(), 'data': scrape_block_packet(blockShipList[ship_port], (ship_port if (ship_port != 13000) else 12000), destination_ip)}
         print("[BlockCache] Cached new block for ship %i, Holding onto it for 5 minutes..." % ship_port)
         return cachedBlocks[ship_port]['data']
 
     last_time = cachedBlocks[ship_port]['time_scraped']
     current_time = time.time()
     if current_time > last_time + (60 * 3):
-        cachedBlocks[ship_port] = {'time_scraped': time.time(), 'data': scrape_block_packet(blockShipList[ship_port], ship_port, destination_ip)}
+        cachedBlocks[ship_port] = {'time_scraped': time.time(), 'data': scrape_block_packet(blockShipList[ship_port], (ship_port if (ship_port != 13000) else 12000), destination_ip)}
         print("[BlockCache] Cached new block for ship %i, Holding onto it for 5 minutes..." % ship_port)
     return cachedBlocks[ship_port]['data']
 
