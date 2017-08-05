1ff48fe9947be5b59ac86f0231d26a13e02199bb
Alam Arias <Alam.GBC@gmail.com>
2017-08-05 15:41:40 -0400

    GChat: remove description:

--- proxy/plugins/GlobalChat.py
+++ proxy/plugins/GlobalChat.py
@@ -134,6 +134,7 @@ if ircMode:
                     return
                 if self.ircOutput is True:
                     if self.nickbuf == user:
+                        msg = msg.replace("description: ", "")
                         print("[GlobalChat] [IRC] <%s> %s" % (self.nickmsgbuf.split("] ")[1], replace_irc_with_pso2(msg).decode('utf-8', 'ignore')))
                     else:
                         print("[GlobalChat] [IRC] <%s> %s" % (user.split("!")[0], replace_irc_with_pso2(msg).decode('utf-8', 'ignore')))
