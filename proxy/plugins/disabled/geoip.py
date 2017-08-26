9eadd6398877b427d105d20fa798e14a1f1c1c1f
Alam Arias <Alam.GBC@gmail.com>
2017-08-25 23:25:48 -0400

    GeoIP: add support of old GeoIP

--- proxy/plugins/disabled/geoip.py
+++ proxy/plugins/disabled/geoip.py
@@ -1,11 +1,22 @@
 import commands
 from config import YAMLConfig
-import geoip2.database
 import json
 import os
 import packetFactory
 from packetFactory import SystemMessagePacket
 import plugins
+
+try:
+    import geoip2.database
+except ImportError:
+    print("[GeoIP] no GeoIP2 library installed")
+
+try:
+    import GeoIP
+except ImportError:
+    print("[GeoIP] no GeoIP library installed")
+
+
 geoiplist = []
 
 GeoSettings = YAMLConfig("cfg/pso2proxy.geoip.config.yml",
@@ -13,34 +24,44 @@ GeoSettings = YAMLConfig("cfg/pso2proxy.geoip.config.yml",
 
 geoipmode = GeoSettings.get_key('enabled')
 
-Countries = None
+geoip2c = None
+geoip1c = None
 
 
 @plugins.on_start_hook
 def load_geoiplist():
     global geoiplist
-    global Countries
+    global geoip2c, geoip1c
     if not os.path.exists("cfg/pso2proxy.geoip.json"):
         f = open("cfg/pso2proxy.geoip.json", "w")
         f.write(json.dumps(geoiplist))
         f.close()
-        print('[Geoip] Blank whitelist made.')
+        print('[GeoIP] Blank whitelist made.')
     else:
         f = open("cfg/pso2proxy.geoip.json", "r")
         geoiplist = json.loads(f.read())
         f.close()
-        print("[Geoip] Loaded %i geoip entries." % len(geoiplist))
+        print("[GeoIP] Loaded %i geoip entries." % len(geoiplist))
+
     try:
-        Countries = geoip2.database.Reader('/var/lib/GeoIP/GeoLite2-Country.mmdb')
+        geoip2c = geoip2.database.Reader('/var/lib/GeoIP/GeoLite2-Country.mmdb')
     except geoip2.FileNotFoundError as e:
-        print("Geoip] GeoIP database not found: %s".format(e))
+        print("[GeoIP] GeoIP2 database not found: %s".format(e))
+    except Exception as e:
+        print("[GeoIP] Unknown error: %s".format(e))
+
+    if geoip2c is None:
+        try:
+            geoip1c = GeoIP.open("/var/lib/GeoIP/GeoLiteCountry.dat", GeoIP.GEOIP_MMAP_CACHE)
+        except Exception as e:
+            print("[GeoIP] GeoIP1 Error: %s".format(e))
 
 
 def save_geoiplist():
     f = open("cfg/pso2proxy.geoip.json", "w")
     f.write(json.dumps(geoiplist))
     f.close()
-    print('[Geoip] Saved whitelist')
+    print('[GeoIP] Saved whitelist')
 
 
 @plugins.CommandHook("geoipmode", "[Admin Only] Toggle geoip mode", True)
@@ -49,19 +70,19 @@ class Whitelistmode(commands.Command):
         global geoipmode
         geoipmode = not geoipmode
         if geoipmode:
-            client.send_crypto_packet(packetFactory.SystemMessagePacket("[Geoip] Whitelist turn on.", 0x3).build())
+            client.send_crypto_packet(packetFactory.SystemMessagePacket("[GeoIP] Whitelist turn on.", 0x3).build())
             return
         else:
-            client.send_crypto_packet(packetFactory.SystemMessagePacket("[Geoip] Whitelist turn off.", 0x3).build())
+            client.send_crypto_packet(packetFactory.SystemMessagePacket("[GeoIP] Whitelist turn off.", 0x3).build())
             return
 
     def call_from_console(self):
         global geoipmode
         geoipmode = not geoipmode
         if geoipmode:
-            return "[Geoip] Whitelist turn on."
+            return "[GeoIP] Whitelist turn on."
         else:
-            return "[Geoip] Whitelist turn off."
+            return "[GeoIP] Whitelist turn off."
 
 
 @plugins.CommandHook("geoip", "[Admin Only] Adds or removes places to the geoip whitelist.", True)
@@ -75,18 +96,18 @@ class Whitelist(commands.Command):
             if params[2] not in geoiplist:
                 geoiplist.append(params[2])
                 save_geoiplist()
-                return "[Geoip] Added %s to the whitelist." % params[2]
+                return "[GeoIP] Added %s to the whitelist." % params[2]
             else:
-                return "[Geoip] %s is already in the whitelist." % params[2]
+                return "[GeoIP] %s is already in the whitelist." % params[2]
         elif params[1] == "del" or params[1] == "DEL":
             if params[2] in geoiplist:
                 geoiplist.remove(params[2])
                 save_geoiplist()
-                return "[Geoip] Removed %s from whitelist." % params[2]
+                return "[GeoIP] Removed %s from whitelist." % params[2]
             else:
-                return "[Geoip] %s is not in the whitelist, can not delete!" % params[2]
+                return "[GeoIP] %s is not in the whitelist, can not delete!" % params[2]
         else:
-            return "[Geoip] Invalid usage. (Usage: whitelist <add/del> <palce>)"
+            return "[GeoIP] Invalid usage. (Usage: whitelist <add/del> <palce>)"
 
     def call_from_client(self, client):
         """
@@ -126,31 +147,43 @@ def whitelist_check(context, data):
 
     :type context: ShipProxy.ShipProxy
     """
-    global Countries
+    global geoip2c, geoip1c
     global geoiplist
     global geoipmode
     if not geoipmode:
         return data
+    place = "NOPE"
     ip = context.transport.getPeer().host
     badip = True
 
-    try:
-        respone = Countries.country(ip)
-        place = respone.country.iso_code
-        if place in geoiplist:
-            badip = False
-    except geoip2.AddressNotFoundError:
-        print("[GeoIP] Could not find %s in GeoIP database)".format(ip))
-        place = "NULL"
-    except Exception as e:
-        print("[GeoIP] Error: %s".format(e))
-        place = "ERROR"
+    if geoip2c:
+        try:
+            respone = geoip2c.country(ip)
+            place = respone.country.iso_code
+            if place in geoiplist:
+                badip = False
+        except geoip2.AddressNotFoundError:
+            print("[GeoIP] Could not find %s in GeoIP database)".format(ip))
+            place = "NULL"
+        except Exception as e:
+            print("[GeoIP] Error: %s".format(e))
+            place = "ERROR"
+    elif geoip1c:
+        try:
+            place = geoip1c.country_code_by_addr(ip)
+            if place in geoiplist:
+                badip = False
+        except Exception as e:
+            print("[GeoIP] Error: %s".format(e))
+            place = "ERROR"
+    else:
+        return data
 
     if ip in geoiplist:
         badip = False
 
     if badip:
-        print("[Geoip] %s (IP: %s) is not in the GeoIP whitelist, disconnecting client." % place, ip)
+        print("[GeoIP] %s (IP: %s) is not in the GeoIP whitelist, disconnecting client." % place, ip)
         context.send_crypto_packet(SystemMessagePacket("You are not on the Geoip whitelist for this proxy, please contact the owner of this proxy.\nDetails:\nCountry Code: %s\nIPv4: %s".format(place, ip), 0x1).build())
         context.transport.loseConnection()
     return data
